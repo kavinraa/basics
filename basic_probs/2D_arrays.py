de898740d4d0600a1856395e6564b6f28c6ad1ff
kavinraa <109820732+kavinraa@users.noreply.github.com>
2024-08-02 16:27:08 +0530

    add_odd_nums

--- basic_probs/2D_arrays.py
+++ basic_probs/2D_arrays.py
@@ -18,25 +18,78 @@
 
 # ---------------------------------------------------------------------------------------------
 
-def flipAndInvertImage(image):
-    # iterate through each rpw in the input matrix
-    for row in image:
-        # flip the row horizontally by reversing it
-        row.reverse()
-        # invert the row 
-        for i in range(len(row)):
-            row[i] = 1 - row[i]
-    return image
+# def flipAndInvertImage(image):
+#     # iterate through each rpw in the input matrix
+#     for row in image:
+#         # flip the row horizontally by reversing it
+#         row.reverse()
+#         # invert the row 
+#         for i in range(len(row)):
+#             row[i] = 1 - row[i]
+#     return image
 
-A = [
-    [1,1,0],
-    [1,0,1],
-    [0,0,0]
-    ]
+# A = [
+#     [1,1,0],
+#     [1,0,1],
+#     [0,0,0]
+#     ]
 
 # result = flipAndInvertImage(A)
-print("Result:")
-for row in result:
-    print(row)
+# print("Result:")
+# for row in result:
+#     print(row)
+
+# ---------------------------------------------------------------------------------------------
+
+# Matrix Diagonal Sum
+
+# def diagonalSum(mat):
+#     n = len(mat)
+#     total = 0
+    
+#     for i in range(n):
+#         # Add the primary diagonal elements
+#             total += mat[i][i]
+#         # Add the primary diagonal elements
+#             total += mat[i][n-i-1]
+    
+  
+#     # If the matrix size is odd, subtract the middle element once
+#     if n % 2 == 1:
+#         total -= mat[n // 2][n // 2]
+    
+#     return total
+
+# # Example usage
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# print(diagonalSum(matrix)) 
+
+# ---------------------------------------------------------------------------------------------
+
+# def find_odd_cells(matrix):
+#     odd_cells = []
+
+#     # Iterate over the rows and columns of the matrix
+#     for i in range(len(matrix)):
+#         for j in range(len(matrix[i])):
+#             if matrix[i][j] % 2 != 0:
+#                 odd_cells.append((i, j))
+    
+#     return odd_cells
+
+# # Example usage
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# odd_cells = find_odd_cells(matrix)
+# print("Cells with odd values:", odd_cells)
 
 # ---------------------------------------------------------------------------------------------
