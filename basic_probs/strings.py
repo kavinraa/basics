5e00ac2e39f488f0afcd8ce09bac18c7f05f7fbe
kavinraa <109820732+kavinraa@users.noreply.github.com>
2024-08-17 04:53:04 +0530

    Update strings.py

--- basic_probs/strings.py
+++ basic_probs/strings.py
@@ -6,7 +6,7 @@ def checkIfPangram(sentence: str) -> bool:
     unique_letters = len(set_sent)
     
     # check if equal to 26, since english alphabets == 26
-    return len(unique_letters) == 26
+    return (unique_letters) == 26
     
 sentence = "thequickbrownfoxjumpsoverthelazydog"
 result  = checkIfPangram(sentence)
