f03c6377faa3e0cc9b9781a9af5545d4c4e3ff3f
kavinraa <109820732+kavinraa@users.noreply.github.com>
2024-11-05 13:32:24 +0530

    armstrong_num

--- basic_probs/sample.py
+++ basic_probs/sample.py
@@ -1,4 +1,4 @@
-# # Find the first digit and last digit
+# # Find the first digit and last digit of a number
 # def firstDigit(n) : 
   
 #     # Remove last digit from number till only one digit is left 
@@ -22,16 +22,15 @@
 # def digit_count(n):
 #     if n == 0 :
 #         return 1
-
+    
 #     count = 0
 #     while n != 0:
-#         n //= 10
+#         n = n // 10
 #         count += 1
-
 #     return count
 
-# n = 3432
-# # print(digit_count(n))
+# n = 23412
+# print(digit_count(n))
 
 # ----------------------------------------------------------------------------------------------
 
@@ -45,15 +44,16 @@
 
 # # Example usage:
 # original_arr = [1, 2, 3, 4, 5]
-# # reverse_array_extra_array(original_arr)
+# reverse_array_extra_array(original_arr)
 
 # ----------------------------------------------------------------------------------------------
 
 # def reverse_number(n):
-#     # Convert the number to a string, reverse it, handle negative sign
+#         # Reverse the string excluding the negative sign and add it back at the front
 #     if n < 0:
 #         reversed_num = -int(str(n)[:0:-1])
 #     else:
+#         # Reverse the string for positive numbers
 #         reversed_num = int(str(n)[::-1])
 #     return reversed_num
 
@@ -79,14 +79,63 @@
 
 # ----------------------------------------------------------------------------------------------
 
-# Python program to find GCD of two numbers
+# # Function to find gcd of two numbers
+# def gcd(a, b):
+#     while b:
+#         a, b = b, a % b
+#     return a
 
+# print(gcd(48, 18))
 
-# Function to find gcd of two numbers
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
+# ----------------------------------------------------------------------------------------------
 
-print(gcd(48, 18))
+# # Print all divisors of a number
+# def divisor(n):
+#     i = 1
+#     while i <= n :
+#         if n % i == 0 :
+#             print(i , end = ' ')
+#         i = i + 1
+
+# print ("The divisors of 100 are: ")
+# divisor(10)
+
+# ----------------------------------------------------------------------------------------------
+
+# def check_if_prime_number(n):
+#     counter = 0
+#     for i in range(1, n+1):
+#         if n % i == 0 :
+#             counter += 1
+#     if counter == 2 :
+#         print("prime number")
+#     else :
+#         print("Not a prime number")
+
+# check_if_prime_number(11)
+
+# ----------------------------------------------------------------------------------------------
+
+# def if_armstrong_number(n):     # sum of the digits raised to the number of digits == original number
+#     # convert number to str for iteration
+#     digits = str(n)
+#     # get the number of digits 
+#     power = len(digits)
+#     #  get the sum of the number 
+#     # total = sum(int(i) ** power for i in digits)
+#     total = 0
+#     for i in digits:
+#         total += int(i) ** power
+
+#     # check if total == original number
+#     return total == n
+
+# num = int(input("enter the number : "))
+# if if_armstrong_number(num):
+#     print(f"{num} is armstrong number")
+# else :
+#     print(f"{num} is not an armstrong number")
+
+# ----------------------------------------------------------------------------------------------
+
 
